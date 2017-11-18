af1167e28c49744ea11db4127973c70b5b2fd23d
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-18 17:28:21 -0500

    added basic routing

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ import store from './app/store';
 import './index.css';
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Root = ({store}) => ( 
+    <Provider store={store}>
+        <Router>
+            <Route path="/" component={App} />
+        </Router>
+    </Provider>
+)
 
 ReactDOM.render(
-    <Provider store={store}><App /></Provider>,
+    ,
     document.getElementById('root'),
 );
 registerServiceWorker();
