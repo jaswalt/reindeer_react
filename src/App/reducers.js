28dea3f5b748cacf857d7788101bc52d43cc3c44
Lex Yehia <lex.yehia@icloud.com>
2017-11-18 16:50:32 -0500

    Added combine reducers function to to top of app

--- src/App/reducers.js
+++ src/App/reducers.js
@@ -1,1 +1,6 @@
-import {  } from "../user/userReducer";
+import { combineReducers } from 'redux';
+import gift from '../gift/giftReducer';
+
+export default combineReducers({
+    gift,
+});
--- src/gift/giftReducer.js
+++ src/gift/giftReducer.js
@@ -2,11 +2,11 @@ const initialState = {
     gifts: [],
 };
 
-export default function giftReducer(state = initialState, action) {
+export default function gift(state = initialState, action) {
     switch (action.type) {
     case 'ADD_GIFT':
         return Object.assign({}, state, {
-            gifts: state.gifts.concat(action.gift),
+            gifts: state.gifts.concat([action.gift]),
         });
     default:
         return state;
