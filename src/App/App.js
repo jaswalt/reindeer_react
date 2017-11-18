359971163dbad7f5439bf11d2ed0964b187896fb
Lex Yehia <lex.yehia@icloud.com>
2017-11-18 17:11:12 -0500

    Added two small '.' to refer to previous folder cause otherwise the system will not work

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import NavBar from './NavBar';
