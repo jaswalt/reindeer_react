4ff7fca3ee5777cff9344a4f002a663d9ca510a9
Lex Yehia <lex.yehia@icloud.com>
2017-11-19 15:52:18 -0500

    Moved test folders in subfolders for each domain component

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers } from 'redux';
 import gift from '../gift/giftReducer';
 
-export default createStore(combineReducers({
+const rootReducer = combineReducers({
     gift,
-}));
+});
+
+export default createStore(rootReducer);
