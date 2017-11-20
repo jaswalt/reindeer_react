1d54c204708bc439f9650a698f1e9163416a2aa6
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-19 20:49:04 -0500

    added user to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import gift from '../gift/giftReducer';
+import user from '../user/userReducer';
 
 const rootReducer = combineReducers({
     gift,
+    user,
 });
 
 export default createStore(rootReducer);
