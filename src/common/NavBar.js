2f05d9193707c5f5900bc054fcae0a4a8c98f2ca
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-21 17:44:39 -0500

    moved the search bar

--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -18,12 +18,15 @@ export default class NavBar extends Component {
         const { isLoggedIn } = this.state;
         return (
             <AppBar
+                style={{ display: 'flex', flex: '0 0 0' }}
                 title="Kaddo"
                 titleStyle={{
                     fontFamily: 'Great Vibes',
+                    flex: '0 0 1',
                 }}
                 showMenuIconButton={false}
                 iconElementRight={isLoggedIn ? <AvatarContainer /> : <Login />}
+                iconStyleRight={{ margin: '0' }}
             >
                 <Search />
             </AppBar>
