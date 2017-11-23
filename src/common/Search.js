8d69827f9613db2a31ed328faf9d522230401029
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-23 18:47:57 -0500

    working on making search bar function

--- src/common/Search.js
+++ src/common/Search.js
@@ -6,16 +6,34 @@ export default class Search extends Component {
         super(props);
 
         this.state = {
+            content: '',
+
         };
+    }
+
+    onContent = (event) => {
+        this.setState({content: event.target})
+    }
+
+    onContentEnter = (event) => {
+        if(event.key == 'Enter'){
+            //this.props.onNewPost('postMessage', this.props.currentUser.name, this.state.content)
+            this.setState({
+              content: ''
+            })
+        }
     }
 
     render() {
         return (
             <SearchBar
-                // onChange={() => console.log('onChange')}
-                // onRequestSearch={() => console.log('onRequestSearch')}
+                spellCheck={true}
+                onChange={this.onContent}
+                onKeyPress={this.onContentEnter}
+                value={this.state.content}
+                //onRequestSearch={this.onContentEnter}
                 hintText="Search for an item"
-                style={{ margin: '3px' }}
+                style={{ margin: '3 px' }}
             />
         );
     }
