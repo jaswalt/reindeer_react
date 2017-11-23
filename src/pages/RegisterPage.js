f58cbd5da528005898efa5cb0a00496c6003f21d
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-23 13:55:01 -0500

    fixes another conflict

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -14,13 +14,9 @@ class LoginPage extends Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
-                <UserRegisterForm />
-=======
                 <UserRegisterForm
                     checkUsername={this.props.checkUsername}
                 />
->>>>>>> 7cc73d0d2af6a4da8ea15e21a908fc32c2e3c6ee
             </div>
         );
     }
@@ -33,13 +29,9 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-<<<<<<< HEAD
-    return { actions: dispatch };
-=======
     return {
         checkUsername: (username) => dispatch(checkUserNameIsValid(username))
     };
->>>>>>> 7cc73d0d2af6a4da8ea15e21a908fc32c2e3c6ee
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
