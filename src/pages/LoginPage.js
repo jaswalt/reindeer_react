c5405f63e220a84790a366b2e6cac4bbe570959a
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-23 19:19:44 -0500

    background image for login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import UserLoginForm from '../user/UserLoginForm';
 
+const imageUrl = 'https://static.pexels.com/photos/257855/pexels-photo-257855.jpeg';
+
+const styles = {
+    container: {
+        backgroundImage: 'url(' + imageUrl + ')',
+        backgroundSize: 'cover',
+        height: '100vh',
+    },
+};
+
 export default class LoginPage extends Component {
     constructor(props) {
         super(props);
