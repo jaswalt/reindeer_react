5c90ab70d820c43b83980acbe442816c9094dc89
Rachael Topp <topp@ualberta.ca>
2017-11-27 15:23:14 -0500

    added random images to wishlists

--- src/pages/Wishlists.js
+++ src/pages/Wishlists.js
@@ -32,6 +32,24 @@ class WishLists extends Component {
     }
 
     render() {
+        const pics = [
+            'http://s1.1zoom.me/big0/449/345838-admin.jpg', 
+            'https://www.legarrick.co.uk/wp-content/uploads/2014/05/xmas-le-garrick-700x400.jpg',
+            'http://www.goodwp.com/images/201211/goodwp.com_25947.jpg',
+            'http://longwallpapers.com/Desktop-Wallpaper/gold-holiday-wallpapers-for-iphone-For-Desktop-Wallpaper.jpg',
+            'http://starliteloungewinebar.com/wp-content/uploads/new-years-glassware-700x614.jpg',
+            'http://www.crossfitbothell.com/wp-content/uploads/2016/12/eating-chocolate-daily-is-good-for-health980-1456212647_980x457.jpg',
+            'http://www.goodwp.com/large/201103/15589.jpg',
+            'https://i.pinimg.com/736x/a5/0a/96/a50a96b6f408404cc71a014652708d77--christmas-wedding-christmas-drinks.jpg',
+            'https://i.pinimg.com/originals/63/c2/90/63c290f0666144c5662d5b374d3bf295.jpg'
+        ]
+
+        // selecting a pic from array of pics
+        const pickAPic = function () {
+        const pic = pics[Math.floor(Math.random() * 8)];
+        return pic;
+        };
+
         return (
             <div style={styles.root}>
                 <GridList
@@ -46,7 +64,7 @@ class WishLists extends Component {
                             subtitle={<span>by <b>{this.props.username}</b></span>}
                             actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
                         >
-                        <img src={wishlist.img} />
+                        <img src={pickAPic()} />
                         </GridTile>
                     ))}
                 </GridList>
