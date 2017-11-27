10a54d03cfe076494f6eda4aea827dec1a76b97d
Rachael Topp <topp@ualberta.ca>
2017-11-26 20:32:06 -0500

    changed avatar dropdown routes

--- src/common/AvatarContainer.js
+++ src/common/AvatarContainer.js
@@ -33,19 +33,19 @@ export default class AvatarContainer extends Component {
                 targetOrigin={{ horizontal: 'right', vertical: 'top' }}
                 anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
             >
-                <Link to="/users/login">
+                <Link to="/user/profile">
                     <MenuItem primaryText="My Profile" style={{ color: '#000' }} />
                 </Link>
-                <Link to="/users/login">
+                <Link to="/gifts">
                     <MenuItem primaryText="My Items" style={{ color: '#000' }} />
                 </Link>
-                <Link to="/users/login">
+                <Link to="/user/wishlists">
                     <MenuItem primaryText="My Lists" style={{ color: '#000' }} />
                 </Link>
-                <Link to="/users/login">
+                <Link to="/user/friends">
                     <MenuItem primaryText="Friends" style={{ color: '#000' }} />
                 </Link>
-                <Link to="/users/login">
+                <Link to="/">
                     <MenuItem primaryText="Logout" style={{ color: '#000' }} />
                 </Link>
             </IconMenu>
