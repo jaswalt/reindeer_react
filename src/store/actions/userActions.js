a992bf5058565c7e53ea61a3ebea0f1c8bdc37ca
Lex Yehia <lex.yehia@icloud.com>
2017-11-27 13:00:21 -0500

    🐛 Adding missing actions
    Adding missinf search users actions

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -45,6 +45,13 @@ export function usernameIsNotValid() {
     };
 }
 
+export function searchUsersSuccess(users) {
+    return {
+        type: types.SEARCH_USERS_SUCCESS,
+        users,
+    }
+}
+
 
 /**
  *
