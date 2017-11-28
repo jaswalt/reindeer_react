0e2776c1e48f895fe5fa73a8a7540b226cac720a
Lex Yehia <lex.yehia@icloud.com>
2017-11-28 11:42:23 -0500

    fix

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import Wishlists from './Wishlists';
 import FriendsList from './FriendsList';
 
