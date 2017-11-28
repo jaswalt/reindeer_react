a0b6c375f6a4923eccf0bb42e3781fcb9111579f
Rachael Topp <topp@ualberta.ca>
2017-11-28 12:00:09 -0500

    small tweaks

--- src/store/actions/wishlistActions.js
+++ src/store/actions/wishlistActions.js
@@ -94,9 +94,9 @@ export function deleteWishlist(userId, wishlistId) {
     };
 }
 
-export function fetchWishlistGifts(wishlistId) {
-    return (dispatch) => {
-        apiGetWishlistGifts(wishlistId)
+export function fetchWishlistGifts() {
+    return (dispatch, getState) => {
+        apiGetWishlistGifts(getState().users.profile.user_id, getState().wishlists.wishlists.pk)
             .then(
                 resp => dispatch(wishlistGiftsFetchSuccess(resp.data)),
                 () => dispatch(wishlistGiftsFetchFailure(true)),
@@ -104,9 +104,9 @@ export function fetchWishlistGifts(wishlistId) {
     };
 }
 
-export function deleteWishlistGift(userId, wishlistId, giftId) {
-    return (dispatch) => {
-        apiDeleteWishlistGift(wishlistId, giftId)
+export function deleteWishlistGift(giftId) {
+    return (dispatch, getState) => {
+        apiDeleteWishlistGift(getState().users.profile.user_id, getState().wishlists.wishlists.pk, giftId)
             .then(() => dispatch(removeGiftFromWishlist(giftId)));
     };
 }
