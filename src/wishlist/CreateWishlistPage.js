70429c8592f579435a6deef402ae15dfaaffe9be
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-29 17:25:08 -0500

    fixes create a wishlist background

--- src/wishlist/CreateWishlistPage.js
+++ src/wishlist/CreateWishlistPage.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react';
 import CreateWishlistForm from './CreateWishlistForm';
-
-const imageUrl = 'https://static.pexels.com/photos/257855/pexels-photo-257855.jpeg';
-
-const styles = {
-    container: {
-        backgroundImage: 'url(' + imageUrl + ')',
-        backgroundSize: 'cover',
-        height: '100vh',
-    },
-};
+import { connect } from 'react-redux';
 
 class CreateWishlistPage extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            theme: {
+                container: {
+                    backgroundImage: 'url(' + this.props.theme.image + ')',
+                    backgroundSize: 'cover',
+                    height: '100vh',
+                    color: 'white',
+                },
+            },
+        };
     }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.theme.image !== nextProps.theme.image) {
+            this.setState({
+                theme: {
+                    container: {
+                        backgroundImage: 'url(' + nextProps.theme.image + ')',
+                        backgroundSize: 'cover',
+                        height: '100vh',
+                        color: 'white',
+                    },
+                },
+            });
+        }
+    }
+
     render() {
         return (
-            <div style={styles.container}>
+            <div style={this.state.theme.container}>
                 <CreateWishlistForm />
             </div>
         );
     }
 }
 
-export default CreateWishlistPage;
+const mapStateToProps = state => ({
+    theme: state.users.activeTheme,
+});
+
+export default connect(mapStateToProps)(CreateWishlistPage);
